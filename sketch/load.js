d9b0ced12113934c824f09233e424f4083f918f6
P@tB <69919456+Code-Me-live@users.noreply.github.com>
2022-07-15 18:48:40 +0200

    ajout overflow auto

--- sketch/load.js
+++ sketch/load.js
@@ -4,6 +4,7 @@ document.addEventListener('click', function(e) {
 });
 
 function clear(e){
+  document.getElementsByTagName("body")[0].style.setProperty("overFlow", "auto", "important");
   if(e.target.id){
     //console.log("ID : " + e.target.id);
     var id = document.getElementById(e.target.id);
