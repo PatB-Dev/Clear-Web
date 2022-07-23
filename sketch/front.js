c404b58578c8d81fb0bbd726e9a4d1d63ecaeff3
P@tB <69919456+Code-Me-live@users.noreply.github.com>
2022-07-23 20:11:45 +0200

    MàJ CSS

--- sketch/front.js
+++ sketch/front.js
@@ -20,3 +20,18 @@ function handleError(error) {
 }
 
 toogle.addEventListener("click", notifyBackgroundPage);
+
+
+var i, checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+function save() {
+    for (i = 0; i < checkboxes.length; i++) {
+        localStorage.setItem(checkboxes[i].value, checkboxes[i].checked);
+    }
+}
+
+function load_() {
+    for (i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = localStorage.getItem(checkboxes[i].value) === 'true' ? true : false;
+    }
+} 
