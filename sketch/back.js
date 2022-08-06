4519136297ba389d8f033a22fa50d6c92b96916b
code-me-live <patricebelda@gmail.com>
2022-08-06 04:50:44 +0200

    Copilot

--- sketch/back.js
+++ sketch/back.js
@@ -6,24 +6,3 @@ function frontMessage(request, sender, sendResponse) {
   sendResponse({ response: toogle.toogleValue });
 }
 browser.runtime.onMessage.addListener(frontMessage);
-
-/*
-document.addEventListener('click', function (e) {
-  window.onload = clear(e);
-});
-
-function clear(e) {
-  //déverrouille de scroll
-  document.getElementsByTagName("body")[0].style.setProperty("overFlow", "auto", "important");
-  if (e.target.id) {
-    //clear sur la class au clique
-    var id = document.getElementById(e.target.id);
-    id.style.setProperty("display", "none", "important");
-  }
-  else {
-    var className = document.getElementsByClassName(e.target.className);
-    //clear sur la class au clique
-    className[0].style.setProperty("display", "none", "important");
-  }
-}
-*/
--- sketch/front.js
+++ sketch/front.js
@@ -1,5 +1,8 @@
 let toogle = document.getElementById("checkbox");
 
+
+
+
 //reception du message du background script au chargement de la page
 window.onload = load();
 function load() {
@@ -8,6 +11,10 @@ function load() {
   getting.then(function (result) {
     toogle.checked = result.toogleValue;
   });
+  if (toogle.checked = true) {
+    console.log('clear ok');
+    clear();
+  }
 }
 
 function sendBack() {
@@ -17,3 +24,26 @@ function sendBack() {
   });
 }
 toogle.addEventListener("click", sendBack);
+
+//execute script nettoyage de page
+//TO DO ajout index page local
+function clear() {
+  document.addEventListener('click', function (e) {
+    window.onload = cleaning(e);
+  });
+
+  function cleaning(e) {
+    //déverrouille de scroll
+    document.getElementsByTagName("body")[0].style.setProperty("overFlow", "auto", "important");
+    if (e.target.id) {
+      //cleaning sur la class au clique
+      var id = document.getElementById(e.target.id);
+      id.style.setProperty("display", "none", "important");
+    }
+    else {
+      var className = document.getElementsByClassName(e.target.className);
+      //cleaning sur la class au clique
+      className[0].style.setProperty("display", "none", "important");
+    }
+  }
+}
