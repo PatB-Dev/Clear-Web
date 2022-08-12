8ab9cb3027099909a365829d3a89df6510e758d2
P@tB <69919456+Code-Me-live@users.noreply.github.com>
2022-08-12 22:11:43 +0200

    Test clen

--- sketch/front.js
+++ sketch/front.js
@@ -1,5 +1,10 @@
 console.log("front.js loaded");
 
+window.document.addEventListener("click", clear);
+function clear(e) {
+  console.log(e.target.id);
+}
+
 //scroll unblock
 document.getElementsByTagName("body")[0].style.setProperty("overFlow", "auto", "important");
 
@@ -23,19 +28,3 @@ function sendBack() {
 }
 toogle.addEventListener("click", sendBack);
 
-
-
-//au clique avoir la class de l'élément cliqué
-document.addEventListener("click", clear);
-function clear(e) {
-  if (e.target.id) {
-    //clear sur la class au clique
-    var id = document.getElementById(e.target.id);
-    id.style.setProperty("display", "none", "important");
-  }
-  else {
-    var className = document.getElementsByClassName(e.target.className);
-    //clear sur la class au clique
-    className[0].style.setProperty("display", "none", "important");
-  }
-}
