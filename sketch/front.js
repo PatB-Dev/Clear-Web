c86263bacc4cf7c69aeadee13232fa75d8f59d90
P@tB <69919456+Code-Me-live@users.noreply.github.com>
2022-10-27 20:18:13 +0200

    Mise à niveau v0

--- sketch/front.js
+++ sketch/front.js
@@ -4,20 +4,11 @@ document.getElementsByTagName("body")[0].style.setProperty("overFlow", "auto", "
 
 document.querySelector('input').addEventListener('change', (e) => {
   localStorage.setItem('toogleValue', e.currentTarget.checked)
+  clear(e)
 })
 
-/*
 function clear(e) {
-  console.log(toogle);
-  if (e.target.id) {
-    //clear sur la class au clique
-    var id = document.getElementById(e.target.id);
-    id.style.setProperty("display", "none", "important");
-  }
-  else {
-    var className = document.getElementsByClassName(e.target.className);
-    //clear sur la class au clique
-    className[0].style.setProperty("display", "none", "important");
-  }
+  console.log(e.target)
 }
-*/
+
+//https://developer.mozilla.org/fr/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessage
