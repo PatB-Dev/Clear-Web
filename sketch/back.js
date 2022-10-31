e2b7a17d81828021696400b1a57cc261e2e092d2
code-me-live <patricebelda@gmail.com>
2022-10-31 19:26:39 +0100

    factorisation et save de l'état du Toogle

--- sketch/back.js
+++ sketch/back.js
@@ -1,16 +1,8 @@
-let toogleV
-
+/*
 browser.runtime.onMessage.addListener(value)
 
 function value(bool) {
-    toogleV = bool.value
-    localStorage.setItem('toogle', bool.value)
+    localStorage.setItem('value', bool.value)
 }
-
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(sender)
-    console.log(toogleV)
-    //sendResponse(true)
-})
-
+*/
 //https://developer.mozilla.org/fr/docs/Mozilla/Add-ons/WebExtensions/Content_scripts
--- sketch/front.js
+++ sketch/front.js
@@ -2,23 +2,22 @@
 document.getElementsByTagName("body")[0].style.setProperty("overFlow", "auto", "important")
 
 document.querySelector('input').addEventListener('change', save)
-
 function save(e) {
-  browser.runtime.sendMessage({ 'value': e.currentTarget.checked })
+  localStorage.setItem('value', e.target.checked)
 }
 
-browser.runtime.sendMessage(
-  'Hello',
-  (response) => {
-    console.log(response)
-  })
+window.addEventListener('load', restore)
+function restore() {
+  let toogle = document.getElementById('checkbox')
+  localStorage.getItem('value') === 'true' ? toogle.checked = true : false
+}
 
 //https://developer.mozilla.org/fr/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessage
 
 /*
-window.document.addEventListener('click', ouEstLeClick);
+document.addEventListener('click', ouEstLeClick);
 function ouEstLeClick(e) {
-  //console.log(window.document.activeElement)
+  console.log(window.document.activeElement)
   console.log(e.target)
 }
 */
