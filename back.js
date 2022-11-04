cb51f7b2bad1efea712d948d8779425d0eafeb6f
code-me-live <patricebelda@gmail.com>
2022-11-04 04:14:50 +0100

    Test 2

---
+++ back.js
@@ -0,0 +1,8 @@
+/*
+browser.runtime.onMessage.addListener(value)
+
+function value(bool) {
+    localStorage.setItem('value', bool.value)
+}
+
+//https://developer.mozilla.org/fr/docs/Mozilla/Add-ons/WebExtensions/Content_scripts
--- sketch/front.js
+++ sketch/front.js
@@ -1,24 +1,24 @@
 //déblocage du scroll
-document.getElementsByTagName("body")[0].style.setProperty("overFlow", "auto", "important")
+document.getElementsByTagName('body')[0].style.setProperty("overFlow", "auto", "important")
+document.querySelector('html').removeAttribute('class')
 
-document.querySelector('input').addEventListener('change', save)
-function save(e) {
+let toogleC = document.getElementById('checkbox')
+
+document.querySelector('input').addEventListener('change', (e) => {
   localStorage.setItem('value', e.target.checked)
-}
+})
 
-let toogle = document.getElementById('checkbox')
-localStorage.getItem('value') === 'true' ? toogle.checked = true : false
-
-/*
-window.addEventListener('load', restore)
-function restore() {
-  //localStorage.getItem('value') === 'true' ? toogle.checked = true : false
-}
-*/
+localStorage.getItem('value') === 'true' ? toogleC.checked = true : false
 
 document.addEventListener('click', (e) => {
-  console.log(toogle.checked)
-  e.target.style.setProperty('display', 'none')
+  console.log(e)
+  const clearHtml = e.currentTarget.body.id
+  console.log(e.currentTarget)
+  if (clearHtml !== 'clear') {
+    console.log('go')
+    e.target.style.setProperty('display', 'none')
+  }
+  //e.target.style.setProperty('display', 'none', 'important')
 })
 
 /*
