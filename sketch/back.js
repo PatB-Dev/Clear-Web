d217bd3ed94b471e050c712b350406299af62984
code-me-live <patricebelda@gmail.com>
2022-11-04 18:48:27 +0100

    supp fichiers inutiles

--- sketch/back.js
+++
@@ -1,8 +0,0 @@
-/*
-browser.runtime.onMessage.addListener(value)
-
-function value(bool) {
-    localStorage.setItem('value', bool.value)
-}
-*/
-//https://developer.mozilla.org/fr/docs/Mozilla/Add-ons/WebExtensions/Content_scripts
