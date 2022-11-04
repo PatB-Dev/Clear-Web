237f623b070486a9404fb2dd1430d49d7255cf86
code-me-live <patricebelda@gmail.com>
2022-11-04 21:48:02 +0100

    test en ligne

--- sketch/front.js
+++ sketch/front.js
@@ -3,33 +3,20 @@ document.getElementsByTagName('body')[0].style.setProperty("overFlow", "auto", "
 document.querySelector('html').removeAttribute('class')
 
 let toogleC = document.getElementById('checkbox')
+var valid
 
 document.querySelector('input').addEventListener('change', (e) => {
   localStorage.setItem('value', e.target.checked)
+  chrome.storage.local.set('value', e.target.checked)
 })
 
 localStorage.getItem('value') === 'true' ? toogleC.checked = true : false
 
+/*
 document.addEventListener('click', (e) => {
   const clearHtml = e.currentTarget.body.id
   if (clearHtml !== 'clear') {
     e.target.style.setProperty('display', 'none', 'important')
   }
-  //e.target.style.setProperty('display', 'none', 'important')
 })
-
-/*
-function clear(e) {
-  if (e.target.id) {
-    //clear sur la class au clique
-    var id = document.getElementById(e.target.id);
-    id.style.setProperty("display", "none", "important");
-  }
-  else {
-    var className = document.getElementsByClassName(e.target.className);
-    //clear sur la class au clique
-    className[0].style.setProperty("display", "none", "important");
-  }
-}
 */
-//https://developer.mozilla.org/fr/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessage
