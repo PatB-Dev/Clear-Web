05ce14d64e6eda7992aaa1ee5f7b6dba6935fbee
code-me-live <patricebelda@gmail.com>
2022-11-06 21:00:02 +0100

    test n°200

--- sketch/front.js
+++ sketch/front.js
@@ -3,19 +3,10 @@ document.getElementsByTagName('body')[0].style.setProperty("overFlow", "auto", "
 document.querySelector('html').removeAttribute('class')
 
 let toogleC = document.getElementById('checkbox')
-let valid
-
-async function getValue() {
-  return localStorage.getItem('value')
-}
 
 document.querySelector('input').addEventListener('change', (e) => {
   localStorage.setItem('value', e.target.checked)
-  valid = getValue
 })
-
-localStorage.getItem('value') === 'true' ? toogleC.checked = true : false
-
 
 document.addEventListener('click', (e) => {
   const clearHtml = e.currentTarget.body.id
@@ -23,3 +14,5 @@ document.addEventListener('click', (e) => {
     e.target.style.setProperty('display', 'none', 'important')
   }
 })
+
+localStorage.getItem('value') === 'true' ? toogleC.checked = true : false
