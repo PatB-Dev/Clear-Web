8f7ba2a6834eece0053d3f3ec6710dc364609102
P@tB <69919456+Code-Me-live@users.noreply.github.com>
2022-11-06 18:30:09 +0100

    debug variable

--- sketch/front.js
+++ sketch/front.js
@@ -3,17 +3,22 @@ document.getElementsByTagName('body')[0].style.setProperty("overFlow", "auto", "
 document.querySelector('html').removeAttribute('class')
 
 let toogleC = document.getElementById('checkbox')
+let valid
+
+async function getValue() {
+  return localStorage.getItem('value')
+}
 
 document.querySelector('input').addEventListener('change', (e) => {
   localStorage.setItem('value', e.target.checked)
-  chrome.storage.local.set('value', e.target.checked)
+  valid = getValue
 })
 
 localStorage.getItem('value') === 'true' ? toogleC.checked = true : false
 
+
 document.addEventListener('click', (e) => {
   const clearHtml = e.currentTarget.body.id
-  console.log(localStorage.getItem('value'))
   if (clearHtml !== 'clear') {
     e.target.style.setProperty('display', 'none', 'important')
   }
